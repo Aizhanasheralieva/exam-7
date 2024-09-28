72dc42b90a933c33f2f77caadaf23d8b614799dd
Sheralieva Aizhana <asheralieva7@gmail.com>
2024-09-28 17:25:12 +0600

    Added style css rules. Added images for cutlery and coffee-cup. Edited interface details.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
 import "./App.css";
 import {useState} from "react";
+import cutleryImage from "./assets/cutlery.png";
+import coffeeCupImage from "./assets/coffee-cup.png";
 
-interface Props {
+interface Item {
+  name: string;
+  price: number;
+  image: string;
+}
+interface OrderItem {
   name: string;
   price: number;
   quantity: number;
 }
 
+const ITEMS: Item[object] = [
+  {name: 'hamburger', price: 80, image: cutleryImage},
+  {name: 'cheeseburger', price: 90, image: cutleryImage},
+  {name: 'fries', price: 45, image: cutleryImage},
+  {name: 'coffee', price: 70, image: coffeeCupImage},
+  {name: 'tea', price: 50, image: coffeeCupImage},
+  {name: 'cola', price: 40, image: coffeeCupImage},
+]
+const object = {
+  name: 0,
+  price: 0,
+
+}
+
+
 const App = () => {
-  const [order, setOrder] = useState<Props[]>([]);
-  const addItem = (itemName: string, itemPrice: number) => {
+  const [order, setOrder] = useState<OrderItem[]>([]);
+  // const [totalPrice,setTotalPrice] = useState<number>(0);
+  const addItem = (itemName: string) => {
     const existingItem = order.find((item) => item.name === itemName);
     if (existingItem) {
       const recentOrder = order.map(item =>
@@ -19,8 +42,12 @@ const App = () => {
     } else {
       setOrder([...order, {name: itemName, price: itemPrice, quantity: 1}]);
     }
+    // setTotalPrice(prevTotal => prevTotal + itemPrice);
   };
 
+  const getItemName = (itemName: string) => {
+    object[itemName]++;
+  };
   return (
     <div className="container">
       <div className="order-details">
@@ -30,7 +57,7 @@ const App = () => {
             : (
               order.map(item => (
                 <div key={item.name} className="order-item">
-                  {item.name} x{item.quantity} {item.price} * {item.quantity} KGS;
+                  {item.name} x{item.quantity} {item.price * item.quantity} KGS;
                 </div>
               )))
           }
@@ -40,12 +67,21 @@ const App = () => {
       </div>
       <div className="add-items">
         <h3>Add Items</h3>
-        <button onClick={() => addItem('Hamburger', 80)}><strong>Hamburger</strong> <div>Price: 80 KGS</div></button>
+        {ITEMS.map(item => (
+          <button key={item.name} type="button"
+            onClick={() => addItem(item.name)}>
+            <img width={40} src={item.image} alt={item.name}
+            onClick={() => getItemName(item.name)}
+            />
+            <p>{item.name.toUpperCase()} {item.price} KGS</p>
+          </button>
+        ))}
 
       </div>
 
     </div>
-  );
+  )
+    ;
 };
 
 
