5d56fb804beba0f26483d6f8ebbda8597f24e87a
Sheralieva Aizhana <asheralieva7@gmail.com>
2024-09-28 18:48:51 +0600

    Added removeItems button. And counted total price for all added items. Edited css styles for remove button and order-info text.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import {useState} from "react";
+import { useState } from "react";
 import cutleryImage from "./assets/cutlery.png";
 import coffeeCupImage from "./assets/coffee-cup.png";
 
@@ -8,81 +8,93 @@ interface Item {
   price: number;
   image: string;
 }
+
 interface OrderItem {
   name: string;
   price: number;
   quantity: number;
 }
 
-const ITEMS: Item[object] = [
-  {name: 'hamburger', price: 80, image: cutleryImage},
-  {name: 'cheeseburger', price: 90, image: cutleryImage},
-  {name: 'fries', price: 45, image: cutleryImage},
-  {name: 'coffee', price: 70, image: coffeeCupImage},
-  {name: 'tea', price: 50, image: coffeeCupImage},
-  {name: 'cola', price: 40, image: coffeeCupImage},
-]
-const object = {
-  name: 0,
-  price: 0,
-
-}
-
+const ITEMS: Item[] = [
+  { name: "hamburger", price: 80, image: cutleryImage },
+  { name: "cheeseburger", price: 90, image: cutleryImage },
+  { name: "fries", price: 45, image: cutleryImage },
+  { name: "coffee", price: 70, image: coffeeCupImage },
+  { name: "tea", price: 50, image: coffeeCupImage },
+  { name: "cola", price: 40, image: coffeeCupImage },
+];
 
 const App = () => {
   const [order, setOrder] = useState<OrderItem[]>([]);
-  // const [totalPrice,setTotalPrice] = useState<number>(0);
-  const addItem = (itemName: string) => {
+  const [totalPrice, setTotalPrice] = useState<number>(0);
+
+  const addItem = (itemName: string, itemPrice: number) => {
     const existingItem = order.find((item) => item.name === itemName);
     if (existingItem) {
-      const recentOrder = order.map(item =>
-        item.name === itemName ? {...item, quantity: item.quantity + 1} : item
+
+      const updatedOrder = order.map((item) =>
+        item.name === itemName
+          ? { ...item, quantity: item.quantity + 1 }
+          : item
       );
-      setOrder(recentOrder);
+      setOrder(updatedOrder);
     } else {
-      setOrder([...order, {name: itemName, price: itemPrice, quantity: 1}]);
+
+      setOrder([...order, { name: itemName, price: itemPrice, quantity: 1 }]);
     }
-    // setTotalPrice(prevTotal => prevTotal + itemPrice);
+    setTotalPrice((prevTotal) => prevTotal + itemPrice);
   };
 
-  const getItemName = (itemName: string) => {
-    object[itemName]++;
+  const removeItem = (itemName: string) => {
+    const itemToRemove = order.find(item => item.name === itemName);
+
+    if (itemToRemove) {
+      const updatedOrder = order.filter(item => item.name !== itemName);
+      setOrder(updatedOrder);
+      setTotalPrice((prevTotal) => prevTotal - itemToRemove.price * itemToRemove.quantity);
+    }
   };
+
   return (
     <div className="container">
       <div className="order-details">
         <h3>Order Details</h3>
         <div id="order-list">
-          {order.length === 0 ? (<div>Order is empty! <br/> Please, add some items!</div>)
-            : (
-              order.map(item => (
-                <div key={item.name} className="order-item">
-                  {item.name} x{item.quantity} {item.price * item.quantity} KGS;
-                </div>
-              )))
-          }
-
+          {order.length === 0 ? (
+            <div className="order-info">
+              Order is empty! <br/> Please, add some items!
+            </div>
+          ) : (
+            order.map((item) => (
+              <div key={item.name} className="order-item">
+                {item.name} x{item.quantity} - {item.price * item.quantity} KGS
+                <button className="remove-btn" onClick={() => removeItem(item.name)}>X</button>
+              </div>
+            ))
+          )}
         </div>
-
+        <hr/>
+        <p><strong>Total price: </strong>{totalPrice} KGS</p>
       </div>
+
       <div className="add-items">
         <h3>Add Items</h3>
-        {ITEMS.map(item => (
-          <button key={item.name} type="button"
-            onClick={() => addItem(item.name)}>
-            <img width={40} src={item.image} alt={item.name}
-            onClick={() => getItemName(item.name)}
-            />
-            <p>{item.name.toUpperCase()} {item.price} KGS</p>
+        {ITEMS.map((item) => (
+          <button
+            key={item.name}
+            type="button"
+            onClick={() => addItem(item.name, item.price)}
+          >
+            <img width={40} src={item.image} alt={item.name}/>
+            <p>
+              {item.name.toUpperCase()}
+              <div>Price: {item.price} KGS</div>
+            </p>
           </button>
         ))}
-
       </div>
-
     </div>
-  )
-    ;
+  );
 };
-
 
 export default App;
